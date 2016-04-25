f4a98cb62deac4c31574592a6371fef0b5f627d1
carmenlm <carmenlopezmedina@yahoo.es>
2016-04-25 23:25:05 +0200

    actualizacion

--- js/myEmployee.js
+++ js/myEmployee.js
@@ -11,24 +11,32 @@ var myEmployees = {
     //metodos
 
     /**
-     *
      * @param {Empleado} empleado -- objeto de clase Empleado
-     * funcion que añade un empleado al array empleados
+     * funcion que añade un empleado al array empleados al final del array
      */
-    addEmpleado: function (empleado) {
+    addEmpleadoLast: function (empleado) {
+        'use stric';
         // hace push al array pasando el objeto
         this.empleados.push(empleado);
-        return false;
+
     },
+    addEmpleadoFirst: function (empleado) {
+        'use stric';
+        //hace unshift al array pasando el objeto
+        this.empleados.unshift(empleado);
+    },
+
 
     /**
      * metodo que muestra todos los empleados
      * @param {nodo DOM} nodo -- parametro nodo del dom para incrustar la cadena con la informacion
      */
     showAllEmpleado: function (nodo) {
+
+        'use stric';
         var cadena = "";
 
-        // recorro todo el array para pasar por todos los objetos
+        // recorro el array para pasar por todos los objetos
         for (var i = 0; i < this.empleados.length; i++) {
             //concateno la cadena llamando al metodo showEmpleado
             cadena += this.showEmpleado(i, nodo);
@@ -45,6 +53,7 @@ var myEmployees = {
      * @param {int} posicion -- entero que pasa una posicion
      */
     showEmpleado: function (posicion) {
+        'use stric';
         var cadena = "<h2>Empleado Número " + (posicion + 1) + "</h2>";
         cadena += "<p>Nombre: " + this.empleados[posicion].nombre + "</p>";
         cadena += "<p>Departamento: " + this.empleados[posicion].departamento + "</p>";
@@ -67,29 +76,46 @@ var Employee = function (name, dep) {
 //funcion de inicializacion del document
 function init() {
 
-    //boton añadir
-    var btnAdd = document.getElementById('add');
+
+    //boton añadir al principio
+    var btnAddFirst = document.getElementById('addFirst');
 
     //evento click del boton añadir
-    btnAdd.onclick = function () {
+    btnAddFirst.addEventListener('click', function () {
         //capturo los datos del formulario
         var nombreCapturado = document.getElementById('nombre').value;
         var departamentoCapturado = document.getElementById('departamento').value;
         //construyo el objeto employee con los datos capturados
         var emp = new Employee(nombreCapturado, departamentoCapturado);
         //llamo al metodo addEmpleado para añadirlo al objeto myEmployee
-        myEmployees.addEmpleado(emp);
-    };
+        myEmployees.addEmpleadoFirst(emp);
+    });
+
+    //boton añadir al principio
+    var btnAddLast = document.getElementById('addLast');
+
+    //evento click del boton añadir
+    btnAddLast.addEventListener('click', function () {
+        //capturo los datos del formulario
+        var nombreCapturado = document.getElementById('nombre').value;
+        var departamentoCapturado = document.getElementById('departamento').value;
+        //construyo el objeto employee con los datos capturados
+        var emp = new Employee(nombreCapturado, departamentoCapturado);
+        //llamo al metodo addEmpleado para añadirlo al objeto myEmployee
+        myEmployees.addEmpleadoLast(emp);
+    });
+
 
     //boton mostrar
     var btnShow = document.getElementById('show');
     //evento click del boton mostrar
-    btnShow.onclick = function () {
+
+    btnShow.addEventListener('click', function () {
         //capturo el nodo donde quiero mostrar la informacion
         var nodoInformacion = document.getElementById('informacion');
         // llamo al metodo de myEmployee para mostrar todos los empleados
         myEmployees.showAllEmpleado(nodoInformacion);
-    };
+    });
 
 }
 
